5dfcc313fa777ed5cf0219986fec45e22cda217b
T00097-RD3(李儼哲) <T00097@senao.com.tw>
2021-02-03 23:35:48 +0800

    建立Service

---
+++ SystemBase.Service/Interfaces/IStaffService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SystemBase.Repository.Models;
+
+namespace SystemBase.Service.Interfaces
+{
+    public interface IStaffService 
+    {
+        Staff GetByName(string name);
+
+        Staff Get(int id);
+
+        int Create(Staff staff);
+
+        bool Update(Staff staff);
+
+        bool Delete(int id);
+    }
+}
---
+++ SystemBase.Service/Services/StaffService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SystemBase.Repository;
+using SystemBase.Repository.Interfaces;
+using SystemBase.Repository.Models;
+using SystemBase.Service.Interfaces;
+
+namespace SystemBase.Service.Services
+{
+    public class StaffService : IStaffService
+    {
+        private readonly IRepository<Staff, int> Reposty;
+
+        public StaffService(IRepository<Staff, int> repository)
+        {
+            Reposty = repository;
+        }
+
+        public Staff GetByName(string name)
+        {
+            var result = Reposty.Find(f => f.Name == name).FirstOrDefault();
+
+            return result ?? new Staff();
+        }
+
+        public Staff Get(int id)
+        {
+            var result = Reposty.FindById(id);
+
+            return result;
+        }
+
+        public int Create(Staff staff)
+        {
+            Reposty.Create(staff);
+
+            return staff.Id;
+        }
+
+        public bool Update(Staff staff)
+        {
+            try
+            {
+                Reposty.Update(staff);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // 例外處理
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                Reposty.Delete(id);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // 例外處理
+                return false;
+            }
+        }
+    }
+}
